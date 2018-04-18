48b60d8896d3f5b7ca427a00bd4b4841fd787a0d
pavan <pavan@CooL>
2018-04-18 19:47:14 +0530

    Boom

--- Eclipse_Java_Project/src/Commit_Test.java
+++
@@ -1,9 +0,0 @@
-
-public class Commit_Test 
-{
-	public static void main(String[] args) 
-	{
-		System.out.println("Only Commiting");
-	}
-
-}
--- Eclipse_Java_Project/src/Exception_Handling.java
+++
@@ -1,9 +0,0 @@
-
-public class Exception_Handling 
-{
-	public static void main(String[] args) 
-	{
-			System.out.println("Test Bumchik");
-	}
-
-}

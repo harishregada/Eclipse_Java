cf07b6b6cb22debeefa176668332e5bc395fb1d1
pavan <pavan@CooL>
2018-04-19 12:37:07 +0530

    This Test 1

---
+++ Eclipse_Java_Project/src/This_Test1.java
@@ -0,0 +1,23 @@
+class Test1
+{
+	Test1()
+	{
+		System.out.println("Test1...");
+	}
+}
+
+class Test2
+{
+	Test2()
+	{
+		System.out.println("Test2...");
+	}
+}
+public class This_Test1 {
+
+	public static void main(String[] args) 
+	{
+		Test1 obj=new Test1();
+	}
+
+}
